69b8704856e29fbc663fcff5dbea614b3fb6e5c7
Peter M. Clausen <petermichaelclausen@gmail.com>
2019-08-18 19:50:43 +0200

    Add hello world exercise with CMake/Ctest
    The "Hello World" exercise is the very first exercise that the student
    sees, so we have added in comments to direct them on what to expect.
    
    Renamed test_end to test_report, this is dependent on an upcoming change
    in the testlib/testermain.f90

--- exercises/hello-world/hello_world_test.f90
+++ exercises/hello-world/hello_world_test.f90
@@ -1,20 +1,16 @@
-
 ! This test was created from ..\..\exercism\problem-specifications\exercises\hello-world\canonical-data.json
 ! version: 1.1.0 
 !
-
-
-
 program hello_world_test_main
   use TesterMain
   use hello_world 
+
   implicit none
 
-    
+  !! About the test comments:
+
   ! Test 1: Say Hi!
   call assert_equal("Hello, World!", hello(), "Say Hi!")
- 
-  call test_end()
- 
+  call test_report()
 end program
- 
+
