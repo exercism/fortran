7c5e2f2827116e735a58a4ba2c024e29fa2e17da
Peter M. Clausen <petermichaelclausen@gmail.com>
2020-03-04 03:13:05 +0100

    Adding nth-prime exercise
    Adding nth-prime exercise

---
+++ exercises/nth-prime/example.f90
@@ -0,0 +1,41 @@
+
+module nth_prime
+  implicit none
+contains
+
+  ! loop from 2 to n/2. If mod(i,n)==0 this is not prime
+  logical pure function is_prime(n)
+
+    integer, intent(in) :: n
+    integer :: i
+
+    is_prime=.true.
+    do i=2,n/2
+      if ( mod(n,i)==0 ) then
+        is_prime = .false.
+        return
+      end if
+    end do
+  end function
+
+  ! get nth prime
+  integer pure function prime(n)
+    integer, intent(in) :: n
+    integer :: i,j 
+    prime = -1
+    if (n<1) then
+      return
+    endif
+    j=0
+    i=1
+    do while (j<n )
+      i=i+1
+      !write(*,*)  'Prime',i,j
+      if (is_prime(i)) then
+        j=j+1
+      endif
+    end do
+    prime=i
+  end function
+
+end module
