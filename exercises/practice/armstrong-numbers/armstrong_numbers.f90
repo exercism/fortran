51dfd1b535881630dcd85f505c94e03e8580bee0
Peter M. Clausen <petermichaelclausen@gmail.com>
2021-05-26 19:15:14 +0200

    Adding implementation for armstrong_numbers

--- exercises/practice/armstrong-numbers/armstrong_numbers.f90
+++ exercises/practice/armstrong-numbers/armstrong_numbers.f90
@@ -3,8 +3,11 @@ module armstrong_numbers
   implicit none
 contains
 
-  logical function dummy()
-    dummy=.true.
+  logical function isArmstrongNumber(i)
+    integer, intent(in) :: i
+    
+    isArmstrongNumber = .false.
+
   end function
 
 end module
