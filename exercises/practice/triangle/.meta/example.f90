75fe64f03306777f2a95b67dc6b0784ff52db605
Peter M. Clausen <petermichaelclausen@gmail.com>
2021-06-09 11:11:37 +0200

    Fixed naming and added stubs

--- exercises/practice/triangle/.meta/example.f90
+++ exercises/practice/triangle/.meta/example.f90
@@ -25,56 +25,56 @@ module triangle
     intol = abs(a-b)<TOL
   end function
 
-  logical function istrianle_real(tria)
+  logical function is_trianle_real(traingle_edges)
     ! sum of 2 sides > other side
-    real,dimension(3) :: tria
-    istrianle_real = sum(tria(1:2))>tria(3) .and. sum(tria(2:3))>tria(1) .and. tria(1)+tria(3)>tria(2)
+    real,dimension(3) :: traingle_edges
+    is_trianle_real = sum(traingle_edges(1:2))>traingle_edges(3) .and. sum(traingle_edges(2:3))>traingle_edges(1) .and. traingle_edges(1)+traingle_edges(3)>traingle_edges(2)
   end function
-  logical function istrianle_int(tria)
+  logical function is_trianle_int(traingle_edges)
     ! sum of 2 sides > other side
-    integer,dimension(3) :: tria
-    istrianle_int = sum(tria(1:2))>tria(3) .and. sum(tria(2:3))>tria(1) .and. tria(1)+tria(3)>tria(2)
+    integer,dimension(3) :: traingle_edges
+    is_trianle_int = sum(traingle_edges(1:2))>traingle_edges(3) .and. sum(traingle_edges(2:3))>traingle_edges(1) .and. traingle_edges(1)+traingle_edges(3)>traingle_edges(2)
   end function
 
-  logical function equilateral_real(tria)
-    real,dimension(3) :: tria
-    equilateral_real= istrianle_real(tria) .and. intol(tria(1),tria(2)) .and.intol(tria(1),tria(3)) .and. .not. intol(tria(1),0.0)
+  logical function equilateral_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
+    equilateral_real= is_trianle_real(traingle_edges) .and. intol(traingle_edges(1),traingle_edges(2)) .and.intol(traingle_edges(1),traingle_edges(3)) .and. .not. intol(traingle_edges(1),0.0)
   end function
 
-  logical function equilateral_int(tria)
-    integer,dimension(3) :: tria
-    equilateral_int=istrianle_int(tria) .and. tria(1)==tria(2) .and.tria(1)==tria(3) .and. tria(1)/=0
+  logical function equilateral_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
+    equilateral_int=is_trianle_int(traingle_edges) .and. traingle_edges(1)==traingle_edges(2) .and.traingle_edges(1)==traingle_edges(3) .and. traingle_edges(1)/=0
   end function
 
-  logical function isosceles_real(tria)
-    real,dimension(3) :: tria
+  logical function isosceles_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
     isosceles_real=.false.
-    if (equilateral_real(tria))then
+    if (equilateral_real(traingle_edges))then
       isosceles_real = .true.
       return
     endif
-    isosceles_real = istrianle_real(tria) .and. intol(tria(1),tria(2)) .or. intol(tria(1),tria(3)) .or. intol(tria(2),tria(3))
+    isosceles_real = is_trianle_real(traingle_edges) .and. intol(traingle_edges(1),traingle_edges(2)) .or. intol(traingle_edges(1),traingle_edges(3)) .or. intol(traingle_edges(2),traingle_edges(3))
   end function
 
-  logical function isosceles_int(tria)
-    integer,dimension(3) :: tria
+  logical function isosceles_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
     isosceles_int=.false.
-    if (equilateral_int(tria))then
+    if (equilateral_int(traingle_edges))then
       isosceles_int = .true.
       return
     endif
-    isosceles_int =istrianle_int(tria) .and. (tria(1)==tria(2) .or. tria(1)==tria(3) .or. tria(2)==tria(3))
+    isosceles_int =is_trianle_int(traingle_edges) .and. (traingle_edges(1)==traingle_edges(2) .or. traingle_edges(1)==traingle_edges(3) .or. traingle_edges(2)==traingle_edges(3))
   end function
 
 
-  logical function scalene_real(tria)
-    real,dimension(3) :: tria
-    scalene_real = istrianle_real(tria) .and. .not. isosceles(tria) .and. .not. equilateral(tria)
+  logical function scalene_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
+    scalene_real = is_trianle_real(traingle_edges) .and. .not. isosceles(traingle_edges) .and. .not. equilateral(traingle_edges)
   end function
 
-  logical function scalene_int(tria)
-    integer,dimension(3) :: tria
-    scalene_int = istrianle_int(tria) .and. .not. isosceles(tria) .and. .not. equilateral(tria)
+  logical function scalene_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
+    scalene_int = is_trianle_int(traingle_edges) .and. .not. isosceles(traingle_edges) .and. .not. equilateral(traingle_edges)
   end function
 
 
--- exercises/practice/triangle/triangle.f90
+++ exercises/practice/triangle/triangle.f90
@@ -2,11 +2,52 @@
 module triangle
   implicit none
 
-  contains
+  interface equilateral
+    module procedure equilateral_real
+    module procedure equilateral_int
+  end interface
 
-  logical function equilateral(tria)
-    integer,dimension(3) :: tria
-    equilateral = .false.
+  interface scalene
+    module procedure scalene_real
+    module procedure scalene_int
+  end interface
+
+  interface isosceles
+    module procedure isosceles_real
+    module procedure isosceles_int
+  end interface
+
+ contains
+
+  logical function equilateral_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
+    equilateral_real = .false.
+  end function
+
+  logical function equilateral_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
+    equilateral_int = .false.
+  end function
+
+  logical function isosceles_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
+    isosceles_real = .false.
+  end function
+
+  logical function isosceles_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
+    isosceles_int = .false.
+  end function
+
+
+  logical function scalene_real(traingle_edges)
+    real,dimension(3) :: traingle_edges
+    scalene_real = .false.
+  end function
+
+  logical function scalene_int(traingle_edges)
+    integer,dimension(3) :: traingle_edges
+    scalene_int = .false.
   end function
 
 end module
