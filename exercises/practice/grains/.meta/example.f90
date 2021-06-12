f99acba57c51609887f7bfffe64f29f4e2aa6087
Peter M. Clausen <petermichaelclausen@gmail.com>
2021-06-12 14:12:34 +0200

    Add stub total and rename nr to 'n'

--- exercises/practice/grains/.meta/example.f90
+++ exercises/practice/grains/.meta/example.f90
@@ -6,11 +6,11 @@ module grains
 contains
 
   ! Using double precision return type instead of int to avoid overflow in int
-  double precision function square(nr)
-    integer :: nr
+  double precision function square(n)
+    integer :: n
     square = -1.d0
-    if (nr>=1 .and. nr<=64) then
-      square = 2.d0**(nr-1)
+    if (n>=1 .and. n<=64) then
+      square = 2.d0**(n-1)
     end if
   end function
 
--- exercises/practice/grains/grains.f90
+++ exercises/practice/grains/grains.f90
@@ -6,9 +6,13 @@ module grains
 contains
 
   ! Using double precision return type instead of int to avoid overflow in int
-  double precision function square(nr)
-    integer :: nr
+  double precision function square(n)
+    integer :: n
     square = -1.d0
+  end function
+
+  double precision function total()
+    total = -1.d0
   end function
 
 end module
