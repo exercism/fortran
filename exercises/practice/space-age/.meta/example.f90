34bc8523eec640daa78cc2500e8dbf4c467b4471
Peter M. Clausen <petermichaelclausen@gmail.com>
2021-06-18 09:02:22 +0200

    Update exercises/practice/space-age/.meta/example.f90
    Co-authored-by: J.R. "hiljusti" Hill <hiljusti@amazon.com>

--- exercises/practice/space-age/.meta/example.f90
+++ exercises/practice/space-age/.meta/example.f90
@@ -1,59 +1,56 @@
 
+module orbital_constants
+  implicit none
+  
+  ! Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+  double precision, parameter, private :: earth_year_seconds = 31557600.d0
+  
+  ! A planet's orbit as measured in Earth years
+  double precision, parameter, private :: mercury_earth_years = 0.2408467d0
+  double precision, parameter, private :: venus_earth_years = 0.61519726d0
+  double precision, parameter, private :: mars_earth_years = 1.8808158d0
+  double precision, parameter, private :: jupiter_earth_years = 11.862615d0
+  double precision, parameter, private :: saturn_earth_years = 29.447498d0
+  double precision, parameter, private :: uranus_earth_years = 84.016846d0
+  double precision, parameter, private :: neptune_earth_years = 164.79132d0
+
+end module orbital_constants
+
 module space_age
+  use orbital_constants
   implicit none
 contains
 
   double precision function age_in_years(planet, seconds)
-    !Mercury: orbital period 0.2408467 Earth years
-    !Venus: orbital period 0.61519726 Earth years
-    !Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
-    !Mars: orbital period 1.8808158 Earth years
-    !Jupiter: orbital period 11.862615 Earth years
-    !Saturn: orbital period 29.447498 Earth years
-    !Uranus: orbital period 84.016846 Earth years
-    !Neptune: orbital period 164.79132 Earth years
+    character(len=*), intent(in) :: planet
+    double precision, intent(in) :: seconds
+    double precision, intent(out) :: age_in_years = 0.d0
 
-    character(len=*) :: planet
-    double precision :: seconds
-    
-    age_in_years = 0.d0
+    ! A multiplier for a number of Earth years for the given planet
+    double precision :: n_earth_years = 0.d0
     
     select case (planet)
     case ("Mercury")
-      !Mercury: orbital period 0.2408467 Earth years
-      age_in_years = seconds/31557600.d0/0.2408467d0
-
+      n_earth_years = mercury_earth_years
     case ("Venus")
-      !Venus: orbital period 0.61519726 Earth years
-      age_in_years = seconds/31557600.d0/0.61519726d0
-
+      n_earth_years = venus_earth_years
     case ("Earth")
-      !Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
-      age_in_years = seconds/31557600.d0
-
+      n_earth_years = 1.d0
     case ("Mars")
-      !Mars: orbital period 1.8808158 Earth years
-      age_in_years = seconds/31557600.d0/1.8808158d0
-
+      n_earth_years = mars_earth_years
     case ("Jupiter")
-      !Jupiter: orbital period 11.862615 Earth years
-      age_in_years = seconds/31557600.d0/11.862615d0
-
+      n_earth_years = jupiter_earth_years
     case ("Saturn")
-      !Saturn: orbital period 29.447498 Earth years
-      age_in_years = seconds/31557600.d0/29.447498d0
-
+      n_earth_years = saturn_earth_years
     case ("Uranus")
-      !Uranus: orbital period 84.016846 Earth years
-      age_in_years = seconds/31557600.d0/84.016846d0
-
+      n_earth_years = uranus_earth_years
     case ("Neptune")
-      !Neptune: orbital period 164.79132 Earth years
-      age_in_years = seconds/31557600.d0/164.79132d0
-
+      n_earth_years = neptune_earth_years
     case default
       write(*,*) 'ERROR: unknown planet: '//trim(planet)
-    end select 
+    end select
+    
+    age_in_years = seconds / earth_year_seconds / n_earth_years
 
   end function
 
