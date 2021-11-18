b3af336dec7b611a9cd30eb4118687db566d9e0b
zmoon <zmoon92@gmail.com>
2021-11-17 19:14:08 -0700

    Document inputs in matrix stub

--- exercises/practice/matrix/matrix.f90
+++ exercises/practice/matrix/matrix.f90
@@ -5,17 +5,23 @@ module matrix
 contains
 
   function row(m, m_dim, i) result(r)
-    integer, intent(in) :: m_dim(2)
+    integer, dimension(2), intent(in) :: m_dim
+    !! Matrix dimensions (nrows, ncols)
     character(len=*), dimension(m_dim(1)), intent(in) :: m
+    !! Matrix as a 1-d array of strings
     integer, intent(in) :: i
+    !! Row index
     integer, dimension(m_dim(2)) :: r
     r(:) = 0
   end function
 
   function column(m, m_dim, j) result(c)
-    integer, intent(in) :: m_dim(2)
+    integer, dimension(2), intent(in) :: m_dim
+    !! Matrix dimensions (nrows, ncols)
     character(len=*), dimension(m_dim(1)), intent(in) :: m
+    !! Matrix as a 1-d array of strings
     integer, intent(in) :: j
+    !! Column index
     integer, dimension(m_dim(1)) :: c
     c(:) = 0
   end function
