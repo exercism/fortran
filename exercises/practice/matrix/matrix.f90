037af1f0809b82b0638b7f0ca5a19714de09959f
zmoon <zmoon92@gmail.com>
2021-11-17 20:58:52 -0700

    Rename args

--- exercises/practice/matrix/matrix.f90
+++ exercises/practice/matrix/matrix.f90
@@ -4,25 +4,25 @@ module matrix
 
 contains
 
-  function row(m, m_dim, i) result(r)
-    integer, dimension(2), intent(in) :: m_dim
+  function row(matrix, dims, i) result(r)
+    integer, dimension(2), intent(in) :: dims
     !! Matrix dimensions (nrows, ncols)
-    character(len=*), dimension(m_dim(1)), intent(in) :: m
+    character(len=*), dimension(dims(1)), intent(in) :: matrix
     !! Matrix as a 1-d array of strings
     integer, intent(in) :: i
     !! Row index
-    integer, dimension(m_dim(2)) :: r
+    integer, dimension(dims(2)) :: r
     r(:) = 0
   end function
 
-  function column(m, m_dim, j) result(c)
-    integer, dimension(2), intent(in) :: m_dim
+  function column(matrix, dims, j) result(c)
+    integer, dimension(2), intent(in) :: dims
     !! Matrix dimensions (nrows, ncols)
-    character(len=*), dimension(m_dim(1)), intent(in) :: m
+    character(len=*), dimension(dims(1)), intent(in) :: matrix
     !! Matrix as a 1-d array of strings
     integer, intent(in) :: j
     !! Column index
-    integer, dimension(m_dim(1)) :: c
+    integer, dimension(dims(1)) :: c
     c(:) = 0
   end function
 
