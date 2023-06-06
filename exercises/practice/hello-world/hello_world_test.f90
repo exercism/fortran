8e70f02561da82afbce74fe183a7c7887c466e25
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-06 13:38:33 +0200

    hello-world: remove unneeded comments

--- exercises/practice/hello-world/hello_world_test.f90
+++ exercises/practice/hello-world/hello_world_test.f90
@@ -4,15 +4,9 @@ program hello_world_test_main
 
    implicit none
 
-   ! The hello_world.f90 must contain a module called "hello_world" and implement
-   ! the function "hello" which must return "Hello, World!".
-
    ! Test 1: Say Hi!
-   ! Explaination: This test expects "Hello, World!" to be returned from the function "hello()"
-   ! and the test description is "Say Hi!"
    call assert_equal("Hello, World!", hello(), "Say Hi!")
 
-   ! This last call is to create a test report.
    call test_report()
 end program
 
