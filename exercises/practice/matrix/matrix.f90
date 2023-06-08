ca807ddc7f2d8b76c82347288b25d60c610ceb9f
Zachary Moon <zmoon92@gmail.com>
2023-06-08 07:35:59 -0400

    Improve matrix stub (#175)
    * Clean up and fix `m` in stub
    
    * Document inputs in matrix stub
    
    * Rename args

--- exercises/practice/matrix/matrix.f90
+++ exercises/practice/matrix/matrix.f90
@@ -4,21 +4,25 @@ module matrix
 
 contains
 
-  function row(m, m_dim, i) result(r)
-    character(len=*) :: m
-    integer, intent(in) :: m_dim(2)
+  function row(matrix, dims, i) result(r)
+    integer, dimension(2), intent(in) :: dims
+    !! Matrix dimensions (nrows, ncols)
+    character(len=*), dimension(dims(1)), intent(in) :: matrix
+    !! Matrix as a 1-d array of strings
     integer, intent(in) :: i
-    integer,dimension(m_dim(2)) :: r
-    integer :: A(m_dim(1),m_dim(2))
+    !! Row index
+    integer, dimension(dims(2)) :: r
     r(:) = 0
   end function
 
-  function column(m, m_dim, j) result(c)
-    character(len=*) :: m
-    integer, intent(in) :: m_dim(2)
+  function column(matrix, dims, j) result(c)
+    integer, dimension(2), intent(in) :: dims
+    !! Matrix dimensions (nrows, ncols)
+    character(len=*), dimension(dims(1)), intent(in) :: matrix
+    !! Matrix as a 1-d array of strings
     integer, intent(in) :: j
-    integer, dimension(m_dim(1)) :: c
-    integer :: A(m_dim(1),m_dim(2))
+    !! Column index
+    integer, dimension(dims(1)) :: c
     c(:) = 0
   end function
 
