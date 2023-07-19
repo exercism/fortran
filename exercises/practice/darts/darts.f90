86d157156eaf22b44e9af2d5060e8edc99c0d4a6
simisc <34340168+simisc@users.noreply.github.com>
2023-07-19 15:45:40 +0100

    Reduce indent to 2 spaces for consistency

--- exercises/practice/darts/darts.f90
+++ exercises/practice/darts/darts.f90
@@ -1,13 +1,13 @@
 module darts
-   implicit none
+  implicit none
 
 contains
 
-   function score(x, y) result(points)
-      real, intent(in):: x, y
-      integer :: points
+  function score(x, y) result(points)
+    real, intent(in):: x, y
+    integer :: points
 
-      points = int(x + y) ! Replace this line with your implementation
-   end function score
+    points = int(x + y) ! Replace this line with your implementation
+  end function score
 
 end module darts
