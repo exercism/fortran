6d32a71b678bc8a8088e9023eeda110c98131efc
simisc <34340168+simisc@users.noreply.github.com>
2023-07-19 15:40:15 +0100

    Reduce indent to 2 spaces for consistency

--- exercises/practice/isbn-verifier/.meta/example.f90
+++ exercises/practice/isbn-verifier/.meta/example.f90
@@ -1,34 +1,34 @@
 module isbn_verifier
-   implicit none
+  implicit none
 
 contains
 
-   function isValid(isbn) result(valid)
-      character(*), intent(in) :: isbn
-      logical :: valid
-      integer :: counter, checksum, i, digit
+  function isValid(isbn) result(valid)
+    character(*), intent(in) :: isbn
+    logical :: valid
+    integer :: counter, checksum, i, digit
 
-      counter = 10
-      checksum = 0
+    counter = 10
+    checksum = 0
 
-      do i = 1, len_trim(isbn)
+    do i = 1, len_trim(isbn)
 
-         if ('0' <= isbn(i:i) .and. isbn(i:i) <= '9') then
-            digit = ichar(isbn(i:i)) - ichar('0')
-            checksum = checksum + counter * digit
-            counter = counter - 1
-         else if (isbn(i:i) == 'X' .and. counter == 1) then
-            checksum = checksum + 10
-            counter = counter - 1
-         else if (isbn(i:i) /= '-') then
-            valid = .false.
-            return
-         end if
+      if ('0' <= isbn(i:i) .and. isbn(i:i) <= '9') then
+        digit = ichar(isbn(i:i)) - ichar('0')
+        checksum = checksum + counter * digit
+        counter = counter - 1
+      else if (isbn(i:i) == 'X' .and. counter == 1) then
+        checksum = checksum + 10
+        counter = counter - 1
+      else if (isbn(i:i) /= '-') then
+        valid = .false.
+        return
+      end if
 
-         if (counter < 0) exit
-      end do
+      if (counter < 0) exit
+    end do
 
-      valid = counter == 0 .and. mod(checksum, 11) == 0
-   end function isValid
+    valid = counter == 0 .and. mod(checksum, 11) == 0
+  end function isValid
 
 end module isbn_verifier
--- exercises/practice/isbn-verifier/isbn_verifier.f90
+++ exercises/practice/isbn-verifier/isbn_verifier.f90
@@ -1,13 +1,13 @@
 module isbn_verifier
-   implicit none
+  implicit none
 
 contains
 
-   function isValid(isbn) result(valid)
-      character(*), intent(in) :: isbn
-      logical :: valid
+  function isValid(isbn) result(valid)
+    character(*), intent(in) :: isbn
+    logical :: valid
 
-      valid = isbn == "123" ! Replace this line with your implementation
-   end function isValid
+    valid = isbn == "123" ! Replace this line with your implementation
+  end function isValid
 
 end module isbn_verifier
