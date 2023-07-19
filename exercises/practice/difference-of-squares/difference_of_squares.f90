88ce3f939d3249d48b06c318b18322c56ac8166a
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-07-19 00:15:53 +0000

    Add missing function prototypes

--- exercises/practice/difference-of-squares/difference_of_squares.f90
+++ exercises/practice/difference-of-squares/difference_of_squares.f90
@@ -2,6 +2,16 @@ module difference_of_squares
   implicit none
 contains
 
+  integer function square_of_sum(n)
+    integer :: n
+
+  end function square_of_sum
+
+  integer function sum_of_squares(n)
+    integer :: n
+
+  end function sum_of_squares
+
   integer function difference(n)
     integer :: n
 
