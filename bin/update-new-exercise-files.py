f6332498a95bbbd222a1398feb4c697c206a0138
Isaac Good <github@isaacgood.com>
2026-07-03 22:17:35 -0700

    Replace the test generator with the Jinja2 generator from awk/bash/jq tracks

--- bin/update-new-exercise-files.py
+++ bin/update-new-exercise-files.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 
 See docs/TEST.md for usage of this script
@@ -37,32 +39,17 @@ $ make
 $ ctest -V
 """
 
-import json
 import argparse
+import json
 import os
 import pathlib
-import re
-import tomllib
-import platform
-
-
-def problem_spec_dir() -> pathlib.Path:
-    """Detect and return the problem specs."""
-    cache_dir = os.getenv("XDG_CACHE_HOME", os.getenv("HOME") + "/.cache")
-    specs = pathlib.Path(cache_dir) / "exercism/configlet/problem-specifications"
-    if specs.exists():
-        return specs
-    cur = pathlib.Path(os.getcwd())
-    for i in cur.parents:
-        if i.name == "problem-specifications":
-            return i
-    raise LookupError("Could not find problem specs")
+import textwrap
 
 
 class Exercise:
     """Exercise holds exercise data."""
 
-    def __init__(self, exercise_path, canonical_path):
+    def __init__(self, exercise_path):
         exercise_path = pathlib.Path(exercise_path)
         if not exercise_path.exists():
             raise ValueError(f"{exercise_path} does not exist")
@@ -72,168 +59,77 @@ class Exercise:
         slug = exercise_path.name
         self.config = json.loads((exercise_path / ".meta" / "config.json").read_text())
 
-        if canonical_path:
-            canonical_path = pathlib.Path(canonical_path)
-        else:
-            canonical_path = problem_spec_dir() / "exercises" / slug / "canonical-data.json"
-        self.canonical_data = json.loads(canonical_path.read_text())
-
         self.slug = slug
         self.name = slug.replace("-", "_")
         self.path = exercise_path
-        self.tests = tomllib.loads((exercise_path / ".meta" / "tests.toml").read_text())
 
-    def write_file(self, filetype, content):
+    def config_file(self, filetype):
         if len(self.config["files"][filetype]) != 1:
             raise ValueError(f"Expected exactly one {filetype} file; got {self.config["files"][filetype]!r}")
-        filepath = self.path / self.config["files"][filetype][0]
-        filepath.write_text(content.strip() + "\n")
+        return self.path / self.config["files"][filetype][0]
+
+    def write_file(self, filepath, content):
+        if filepath.exists():
+            print(f"{filepath} already exists; will not replace.")
+        else:
+            filepath.write_text(content.strip() + "\n")
+            print(f"Wrote to {filepath}")
 
     def write_stub_file(self, content):
-        self.write_file("solution", content)
+        self.write_file(self.config_file("solution"), content)
 
     def write_example_file(self, content):
-        self.write_file("example", content)
+        self.write_file(self.config_file("example"), content)
 
-    def write_test_file(self, content):
-        self.write_file("test", content)
-
-    def flatten_test_cases(self, cases):
-        """Recursive flatten test cases, returning individual cases with parent descriptions."""
-        for case_or_group in cases:
-            if "cases" in case_or_group:
-                for groups, child_case in self.flatten_test_cases(case_or_group["cases"]):
-                    yield ([case_or_group["description"]] + groups, child_case)
-            else:
-                yield ([], case_or_group)
-
-    def test_cases(self):
-        reimplemented = {
-            test["reimplements"]
-            for test in self.tests.values()
-            if test.get("include", True) and "reimplements" in test
-        }
-        cases = []
-        tests = self.tests
-        for groups, case in self.flatten_test_cases(self.canonical_data["cases"]):
-            # Filter out test cases with include=false or not listed.
-            if case["uuid"] not in tests or case["uuid"] in reimplemented:
-                continue
-            if not tests[case["uuid"]].get("include", True):
-                continue
-            # Add parent descriptions.
-            case["descriptions"] = groups + [case["description"]]
-            cases.append(case)
-        return cases
-
-
-def fix_and_quote_fortran_multiline(txt):
-    """Fortran can't handle multiline strings, so adding a continuation character '&' if necessary."""
-    if isinstance(txt, str):
-        txt = txt.replace("\n", '"// & \n    & "')
-        return '"%s"' % txt
-    return txt
-
-
-def write_testcase(c, TEST_NUMBER):
-    """Putting the test case together.
-    Format:
-    ! Test <TEST_NUMBER>: (description of test)
-    call assert.... (actual test case)
-    """
-    si = []
-    # example data
-    # { "description": "non-question ending with whitespace",
-    #   "property": "response",
-    #   "input": {"heyBob": "This is a statement ending with whitespace      "},
-    #   "expected": "Whatever."}
-    description = c["description"]
-    fcall = c["property"]
-    error = c["expected"]["error"] if type(
-        c["expected"]) is dict and "error" in c["expected"] else None
-    fargs = [v for v in c["input"].values()]
-    if fargs:
-        inp = f"{fcall}({fix_and_quote_fortran_multiline(fargs[0])}"
-    else:
-        inp = f"{fcall}({fix_and_quote_fortran_multiline(" ")}"  # empty array
-    for a in fargs[1:]:
-        inp = f"{inp}, {fix_and_quote_fortran_multiline(a)}"
-    inp = f"{inp})"
-    expected = c["expected"]
-    if expected == True:
-        expected = ".true."
-    elif expected == False:
-        expected = ".false."
-    elif expected == "error":
-        expected = ".false."
-    else:
-        expected = fix_and_quote_fortran_multiline(expected)
-    si.append("  ! Test %d: %s" % (TEST_NUMBER, description))
-    if error:
-        expected = "ERROR"
-        si.append("  ! ERROR: %s" % (error))
-    si.append(f'  call assert_equal({expected}, {inp}, "{description}")')
-    si.append("")
-    return si
-
-
-def create_single_test(test_cases):
-    """Walk through the json cases and recursively write the test cases"""
-    si = []
-    for i, c in enumerate(test_cases, start=1):
-        si.extend(write_testcase(c, i))
-    return si
+    def write_test_template(self, content):
+        filepath = self.path / ".meta/template.j2"
+        self.write_file(filepath, content)
 
 
 def stub_implementation(exercise_name: str) -> str:
-    return f"""module {exercise_name}
-  implicit none
-contains
+    return textwrap.dedent("""\
+        module {exercise_name}
+          implicit none
+        contains
 
-  logical function dummy()
-    dummy=.true.
-  end function
+          logical function dummy()
+            dummy=.true.
+          end function
 
-end module
-"""
+        end module
+    """)
 
 
-def test_implementation(exercise):
-    """Main function writing header and footer of the Fortran test file"""
-    header = f"! The tests were created from https://github.com/exercism/problem-specifications/blob/main/exercises/{exercise.slug}/canonical-data.json\n"
+def template_stub() -> str:
+    return textwrap.dedent("""\
+        {{ header }}
 
-    program = f"""\
-program {exercise.name}_test_main
-  use TesterMain
-  use {exercise.name}
-  implicit none
+        program {{ name }}_test_main
+          use TesterMain
+          use {{ name }}
+          implicit none
 
-"""
+        {% for case in cases -%}
+          ! Test {{ loop.index }}: {{ case["description"] }}
+          call assert_equal("{{ case["expected"] }}", {{ case["property"] }}(...), "{{ case["description"] }}")
 
-    s = header + program
-    si = s.splitlines()
-    si.extend(create_single_test(exercise.test_cases()))
+        {% endfor %}
 
-    si.append("  call test_report()")
-    si.append("")
-    si.append("end program")
-    si.append("")
+          call test_report()
 
-    return "\n".join(si)
+        end program
+    """)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create exercise")
     parser.add_argument("-e", "--exercise_path", help="The path of the exercise")
-    # parser.add_argument("exercise_name", help="The name of the exercise")
-    # parser.add_argument("stub_file_path", help="The stub file"s path")
-    # parser.add_argument("test_file_path", help="The test file"s path")
-    # parser.add_argument("example_file_path", help="The example file"s path")
-    parser.add_argument("--canonical_data_file_path", help="The canonical data file's path", required=False)
 
     args = parser.parse_args()
-    exercise = Exercise(args.exercise_path, args.canonical_data_file_path)
+    exercise = Exercise(args.exercise_path)
 
     exercise.write_stub_file(stub_implementation(exercise.name))
     exercise.write_example_file(stub_implementation(exercise.name))
-    exercise.write_test_file(test_implementation(exercise))
+    exercise.write_test_template(template_stub())
+
+    print(f"Use './bin/generate_tests {exercise.slug}' to generate the test file from the Jinja template ({exercise.path / ".meta/template.j2"}).")
--- exercises/practice/bob/bob_test.f90
+++ exercises/practice/bob/bob_test.f90
@@ -1,95 +1,146 @@
+! The tests were created from https://github.com/exercism/problem-specifications/blob/main/exercises/bob/canonical-data.json
+
 program bob_test_main
-   use TesterMain
-   use bob
+  use TesterMain
+  use bob
+  implicit none
 
-   implicit none
+  character(len=1), parameter :: CARRIAGE_RETURN = CHAR(13)
+  character(len=1), parameter :: NEWLINE = CHAR(10)
+  character(len=1), parameter :: TAB = CHAR(9)
 
-    character(len=1), parameter :: CARRIAGE_RETURN = CHAR(13)
-    character(len=1), parameter :: NEWLINE = CHAR(10)
-    character(len=1), parameter :: TAB = CHAR(9)
+! Test 1: asking a question
+  call assert_equal("Sure.", &
+                    hey("Does this cryogenic chamber make me look fat?"), &
+                    "asking a question")
 
-   ! Test 1: stating something
-   call assert_equal("Whatever.", hey("Tom-ay-to, tom-aaaah-to."), "stating something")
+! Test 2: shouting
+  call assert_equal("Whoa, chill out!", &
+                    hey("WATCH OUT!"), &
+                    "shouting")
 
-   ! Test 2: shouting
-   call assert_equal("Whoa, chill out!", hey("WATCH OUT!"), "shouting")
+! Test 3: forceful question
+  call assert_equal("Calm down, I know what I'm doing!", &
+                    hey("WHAT'S GOING ON?"), &
+                    "forceful question")
 
-   ! Test 3: shouting gibberish
-   call assert_equal("Whoa, chill out!", hey("FCECDFCAAB"), "shouting gibberish")
+! Test 4: silence
+  call assert_equal("Fine. Be that way!", &
+                    hey(""), &
+                    "silence")
 
-   ! Test 4: asking a question
-   call assert_equal("Sure.", hey("Does this cryogenic chamber make me look fat?"), "asking a question")
+! Test 5: stating something
+  call assert_equal("Whatever.", &
+                    hey("Tom-ay-to, tom-aaaah-to."), &
+                    "stating something")
 
-   ! Test 5: asking a numeric question
-   call assert_equal("Sure.", hey("You are, what, like 15?"), "asking a numeric question")
+! Test 6: asking a numeric question
+  call assert_equal("Sure.", &
+                    hey("You are, what, like 15?"), &
+                    "asking a numeric question")
 
-   ! Test 6: asking gibberish
-   call assert_equal("Sure.", hey("fffbbcbeab?"), "asking gibberish")
+! Test 7: asking gibberish
+  call assert_equal("Sure.", &
+                    hey("fffbbcbeab?"), &
+                    "asking gibberish")
 
-   ! Test 7: talking forcefully
-   call assert_equal("Whatever.", hey("Let's go make out behind the gym!"), "talking forcefully")
+! Test 8: question with no letters
+  call assert_equal("Sure.", &
+                    hey("4?"), &
+                    "question with no letters")
 
-   ! Test 8: using acronyms in regular speech
-   call assert_equal("Whatever.", hey("It's OK if you don't want to go to the DMV."), "using acronyms in regular speech")
+! Test 9: non-letters with question
+  call assert_equal("Sure.", &
+                    hey(":) ?"), &
+                    "non-letters with question")
 
-   ! Test 9: forceful question
-   call assert_equal("Calm down, I know what I'm doing!", hey("WHAT THE HELL WERE YOU THINKING?"), "forceful question")
+! Test 10: prattling on
+  call assert_equal("Sure.", &
+                    hey("Wait! Hang on. Are you going to be OK?"), &
+                    "prattling on")
 
-   ! Test 10: shouting numbers
-   call assert_equal("Whoa, chill out!", hey("1, 2, 3 GO!"), "shouting numbers")
+! Test 11: ending with whitespace
+  call assert_equal("Sure.", &
+                    hey("Okay if like my  spacebar  quite a bit?   "), &
+                    "ending with whitespace")
 
-   ! Test 11: no letters
-   call assert_equal("Whatever.", hey("1, 2, 3"), "no letters")
+! Test 12: multiple line question
+  call assert_equal("Sure.", &
+                    hey("" // NEWLINE // "Does this cryogenic chamber make" // NEWLINE // " me look fat?"), &
+                    "multiple line question")
 
-   ! Test 12: question with no letters
-   call assert_equal("Sure.", hey("4?"), "question with no letters")
+! Test 13: shouting gibberish
+  call assert_equal("Whoa, chill out!", &
+                    hey("FCECDFCAAB"), &
+                    "shouting gibberish")
 
-   ! Test 13: shouting with special characters
-   call assert_equal("Whoa, chill out!", hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), "shouting with special characters")
+! Test 14: shouting a statement containing a question mark
+  call assert_equal("Whoa, chill out!", &
+                    hey("DO LIONS EAT PEOPLE? AHHHHH."), &
+                    "shouting a statement containing a question mark")
 
-   ! Test 14: shouting with no exclamation mark
-   call assert_equal("Whoa, chill out!", hey("I HATE THE DMV"), "shouting with no exclamation mark")
+! Test 15: shouting numbers
+  call assert_equal("Whoa, chill out!", &
+                    hey("1, 2, 3 GO!"), &
+                    "shouting numbers")
 
-   ! Test 15: statement containing question mark
-   call assert_equal("Whatever.", hey("Ending with ? means a question."), "statement containing question mark")
+! Test 16: shouting with special characters
+  call assert_equal("Whoa, chill out!", &
+                    hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"), &
+                    "shouting with special characters")
 
-   ! Test 16: non-letters with question
-   call assert_equal("Sure.", hey(":) ?"), "non-letters with question")
+! Test 17: shouting with no exclamation mark
+  call assert_equal("Whoa, chill out!", &
+                    hey("I HATE THE DENTIST"), &
+                    "shouting with no exclamation mark")
 
-   ! Test 17: prattling on
-   call assert_equal("Sure.", hey("Wait! Hang on. Are you going to be OK?"), "prattling on")
+! Test 18: prolonged silence
+  call assert_equal("Fine. Be that way!", &
+                    hey("          "), &
+                    "prolonged silence")
 
-   ! Test 18: silence
-   call assert_equal("Fine. Be that way!", hey(""), "silence")
+! Test 19: alternate silence
+  call assert_equal("Fine. Be that way!", &
+                    hey("" // TAB // TAB // TAB // TAB // TAB // TAB // TAB // TAB // TAB // TAB // ""), &
+                    "alternate silence")
 
-   ! Test 19: prolonged silence
-   call assert_equal("Fine. Be that way!", hey("          "), "prolonged silence")
+! Test 20: other whitespace
+  call assert_equal("Fine. Be that way!", &
+                    hey("" // NEWLINE // CARRIAGE_RETURN // " " // TAB // ""), &
+                    "other whitespace")
 
-   ! Test 20: alternate silence
-   call assert_equal("Fine. Be that way!", hey(TAB // TAB // & 
-                                              & TAB // TAB // &
-                                              & TAB // TAB // & 
-                                              & TAB // TAB // &
-                                              & TAB // TAB), "alternate silence")
+! Test 21: talking forcefully
+  call assert_equal("Whatever.", &
+                    hey("Hi there!"), &
+                    "talking forcefully")
 
-   ! Test 21: multiple line question
-   call assert_equal("Sure.", hey( NEWLINE // &
-   & "Does this cryogenic chamber make"// & 
-   & NEWLINE // "me look fat?"), "multiple line question")
+! Test 22: using acronyms in regular speech
+  call assert_equal("Whatever.", &
+                    hey("It's OK if you don't want to go work for NASA."), &
+                    "using acronyms in regular speech")
 
-   ! Test 22: starting with whitespace
-   call assert_equal("Whatever.", hey("         hmmmmmmm..."), "starting with whitespace")
+! Test 23: no letters
+  call assert_equal("Whatever.", &
+                    hey("1, 2, 3"), &
+                    "no letters")
 
-   ! Test 23: ending with whitespace
-   call assert_equal("Sure.", hey("Okay if like my  spacebar  quite a bit?   "), "ending with whitespace")
+! Test 24: statement containing question mark
+  call assert_equal("Whatever.", &
+                    hey("Ending with ? means a question."), &
+                    "statement containing question mark")
 
-   ! Test 24: other whitespace
-   call assert_equal("Fine. Be that way!", hey(NEWLINE // CARRIAGE_RETURN // " " // TAB), "other whitespace")
+! Test 25: starting with whitespace
+  call assert_equal("Whatever.", &
+                    hey("         hmmmmmmm..."), &
+                    "starting with whitespace")
 
-   ! Test 25: non-question ending with whitespace
-   call assert_equal("Whatever.", hey("This is a statement ending with whitespace      "), "non-question ending with whitespace")
+! Test 26: non-question ending with whitespace
+  call assert_equal("Whatever.", &
+                    hey("This is a statement ending with whitespace      "), &
+                    "non-question ending with whitespace")
 
-   call test_report()
+
+
+  call test_report()
 
 end program
-
