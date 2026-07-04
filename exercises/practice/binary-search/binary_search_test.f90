be5a0a0115b63e47d52d04c7f71b48dbc6a0e471
Isaac Good <github@isaacgood.com>
2026-07-04 16:15:39 -0700

    [binary-search] Add a Jinja test template

--- exercises/practice/binary-search/binary_search_test.f90
+++ exercises/practice/binary-search/binary_search_test.f90
@@ -1,6 +1,9 @@
+! The tests were created from https://github.com/exercism/problem-specifications/blob/main/exercises/binary-search/canonical-data.json
+
 program binary_search_test_main
   use TesterMain
   use binary_search
+
   implicit none
 
   ! Test 1: finds a value in an array with one element
@@ -22,26 +25,20 @@ program binary_search_test_main
   call assert_equal(6, find([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21), "finds a value in an array of even length")
 
   ! Test 7: identifies that a value is not included in the array
-  ! ERROR: value not in array
   call assert_equal(-1, find([1, 3, 4, 6, 8, 9, 11], 7), "identifies that a value is not included in the array")
 
   ! Test 8: a value smaller than the array's smallest value is not found
-  ! ERROR: value not in array
   call assert_equal(-1, find([1, 3, 4, 6, 8, 9, 11], 0), "a value smaller than the array's smallest value is not found")
 
   ! Test 9: a value larger than the array's largest value is not found
-  ! ERROR: value not in array
   call assert_equal(-1, find([1, 3, 4, 6, 8, 9, 11], 13), "a value larger than the array's largest value is not found")
 
   ! Test 10: nothing is found in an empty array
-  ! ERROR: value not in array
-  call assert_equal(-1, find([integer ::], 1), "nothing is found in an empty array")
+  call assert_equal(-1, find([], 1), "nothing is found in an empty array")
 
   ! Test 11: nothing is found when the left and right bounds cross
-  ! ERROR: value not in array
   call assert_equal(-1, find([1, 2], 0), "nothing is found when the left and right bounds cross")
 
   call test_report()
 
 end program
-
