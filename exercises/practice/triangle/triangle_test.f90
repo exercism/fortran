e0b702c1f7e1719c80cfe2fa24a558f3390370d5
Isaac Good <github@isaacgood.com>
2026-07-04 19:12:40 -0700

    [triangle] Add a Jinja test template

--- exercises/practice/triangle/triangle_test.f90
+++ exercises/practice/triangle/triangle_test.f90
@@ -1,3 +1,5 @@
+! The tests were created from https://github.com/exercism/problem-specifications/blob/main/exercises/triangle/canonical-data.json
+
 program triangle_test_main
    use TesterMain
    use triangle
@@ -19,49 +21,54 @@ program triangle_test_main
    ! Test 5: sides may be floats
    call assert_equal(.true., equilateral([0.5, 0.5, 0.5]), "sides may be floats")
 
-   ! Test 1: last two sides are equal
+   ! Test 6: last two sides are equal
    call assert_equal(.true., isosceles([3, 4, 4]), "last two sides are equal")
 
-   ! Test 2: first two sides are equal
+   ! Test 7: first two sides are equal
    call assert_equal(.true., isosceles([4, 4, 3]), "first two sides are equal")
 
-   ! Test 3: first and last sides are equal
+   ! Test 8: first and last sides are equal
    call assert_equal(.true., isosceles([4, 3, 4]), "first and last sides are equal")
 
-   ! Test 4: equilateral triangles are also isosceles
+   ! Test 9: equilateral triangles are also isosceles
    call assert_equal(.true., isosceles([4, 4, 4]), "equilateral triangles are also isosceles")
 
-   ! Test 5: no sides are equal
+   ! Test 10: no sides are equal
    call assert_equal(.false., isosceles([2, 3, 4]), "no sides are equal")
 
-   ! Test 6: first triangle inequality violation
+   ! Test 11: first triangle inequality violation
    call assert_equal(.false., isosceles([1, 1, 3]), "first triangle inequality violation")
 
-   ! Test 7: second triangle inequality violation
+   ! Test 12: second triangle inequality violation
    call assert_equal(.false., isosceles([1, 3, 1]), "second triangle inequality violation")
 
-   ! Test 8: third triangle inequality violation
+   ! Test 13: third triangle inequality violation
    call assert_equal(.false., isosceles([3, 1, 1]), "third triangle inequality violation")
 
-   ! Test 9: sides may be floats
+   ! Test 14: sides may be floats
    call assert_equal(.true., isosceles([0.5, 0.4, 0.5]), "sides may be floats")
 
-   ! Test 1: no sides are equal
+   ! Test 15: no sides are equal
    call assert_equal(.true., scalene([5, 4, 6]), "no sides are equal")
 
-   ! Test 2: all sides are equal
+   ! Test 16: all sides are equal
    call assert_equal(.false., scalene([4, 4, 4]), "all sides are equal")
 
-   ! Test 3: two sides are equal
-   call assert_equal(.false., scalene([4, 4, 3]), "two sides are equal")
+   ! Test 17: first and second sides are equal
+   call assert_equal(.false., scalene([4, 4, 3]), "first and second sides are equal")
 
-   ! Test 4: may not violate triangle inequality
+   ! Test 18: first and third sides are equal
+   call assert_equal(.false., scalene([3, 4, 3]), "first and third sides are equal")
+
+   ! Test 19: second and third sides are equal
+   call assert_equal(.false., scalene([4, 3, 3]), "second and third sides are equal")
+
+   ! Test 20: may not violate triangle inequality
    call assert_equal(.false., scalene([7, 3, 2]), "may not violate triangle inequality")
 
-   ! Test 5: sides may be floats
+   ! Test 21: sides may be floats
    call assert_equal(.true., scalene([0.5, 0.4, 0.6]), "sides may be floats")
 
    call test_report()
 
 end program
-
