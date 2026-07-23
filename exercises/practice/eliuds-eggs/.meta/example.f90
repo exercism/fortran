3457248455e5fe37349519b7eccdb104bc846922
Colin Leach <colin.leach@comcast.net>
2026-07-23 08:13:03 -0700

    add another example solution to eliuds-eggs

--- exercises/practice/eliuds-eggs/.meta/example.f90
+++ exercises/practice/eliuds-eggs/.meta/example.f90
@@ -4,11 +4,24 @@ module eliuds_eggs
 
 contains
 
-! Just a cheat implementation at this stage, for validation
+! A cheat implementation, for quick CI validation
   integer function eggCount(number)
     integer :: number
 
     eggCount = popcnt(number)
   end function
+
+! Doing it "properly"
+  ! integer function eggCount(number)
+  !   integer :: number, remaining
+
+  !   eggCount = 0
+  !   remaining = number
+  !   do while (remaining > 0)
+  !     eggCount = eggCount + iand(remaining, 1)
+  !     remaining = shiftr(remaining, 1)
+  !   end do
+
+  ! end function
 
 end module
